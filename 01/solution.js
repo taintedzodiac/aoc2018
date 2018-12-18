44e35afd413fc7d3fca34c376e2474b8ded274c1
Rob Zimmerman <taintedz@gmail.com>
2018-12-18 10:21:11 -0500

    Initial commit of day 1, with inputs for day 2

---
+++ 01/solution.js
@@ -0,0 +1,34 @@
+var input = require('fs').readFileSync('input.txt').toString().split('\n');
+
+var inputArray = [...input].map(n => parseInt(n));
+
+// Part 1
+
+console.log(inputArray.reduce((sum, n) => sum + n));
+
+// Part 2
+
+const frequencies = {};
+let currentFrequency = 0;
+let found = false;
+
+function findFrequencies(inputs, currentFrequency) {
+  for (let frequencyChange of inputs) {
+    if (frequencies[currentFrequency]) {
+      found = currentFrequency;
+      break;
+    }
+
+    frequencies[currentFrequency] = true;
+
+    currentFrequency += frequencyChange;
+  }
+
+  return currentFrequency;
+}
+
+while (!found) {
+  currentFrequency = findFrequencies(inputArray, currentFrequency);
+}
+
+console.log(found);
