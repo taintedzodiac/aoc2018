1099c808a5fc626114c1adbd2437631a867ff6bb
Rob Zimmerman <robz@fogcreek.com>
2018-12-19 10:31:11 -0500

    Day 3 Part 2

--- 03/solution.js
+++ 03/solution.js
@@ -2,6 +2,7 @@ const input = require('fs').readFileSync('input.txt').toString().split('\n');
 
 const inputArray = [...input];
 
+const regexClaimNumber = new RegExp(/(?!^#)\d+/g);
 const regexOrigin = new RegExp(/(?!\s)\d+,\d+/g);
 const regexSize = new RegExp(/(?!\s)\d+x\d+/g);
 
@@ -9,6 +10,8 @@ const coveredSquares = {};
 
 function parseClaim(rawClaim) {
     const parsedClaim = {};
+
+    parsedClaim.number = parseInt(rawClaim.match(regexClaimNumber)[0]);
 
     const origin = rawClaim.match(regexOrigin)[0].split(',');
     parsedClaim.originX = parseInt(origin[0]);
@@ -28,8 +31,8 @@ function coverSquares(claim) {
     for (const x of claimedX) {
         for (const y of claimedY) {
             const key = `${x},${y}`;
-            if (!coveredSquares[key]) coveredSquares[key] = 0;
-            coveredSquares[key]++;
+            if (!coveredSquares[key]) coveredSquares[key] = [];
+            coveredSquares[key].push(claim.number);
         }
     }
 }
@@ -39,8 +42,25 @@ inputArray.forEach(claim => {
     coverSquares(parsedClaim);
 });
 
+const claimsBySquare = [];
+
 Object.keys(coveredSquares).forEach(key => {
-  if (coveredSquares[key] < 2) delete coveredSquares[key];
+  claimsBySquare.push(coveredSquares[key]);
 });
 
-console.log(Object.keys(coveredSquares).length);
+const multipleClaims = claimsBySquare.filter(claim => claim.length > 1);
+
+// Part 1
+
+console.log(multipleClaims.length);
+
+// Part 2
+
+let claimsWithoutOverlap = inputArray.map((claim, i) => i + 1);
+
+for (const claims of multipleClaims) {
+    claimsWithoutOverlap = claimsWithoutOverlap.filter(claim => !claims.includes(claim));
+}
+
+console.log(...claimsWithoutOverlap);
+
